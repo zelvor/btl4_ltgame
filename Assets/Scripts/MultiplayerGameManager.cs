68da439b23e0dde11fc09a32ed2f621e84a87c95
Hai Nguyen Dang <76462148+zelvor@users.noreply.github.com>
2022-11-22 21:23:28 +0700

    update multiboard

--- Assets/Scripts/MultiplayerGameManager.cs
+++ Assets/Scripts/MultiplayerGameManager.cs
@@ -40,34 +40,106 @@ public class MultiplayerGameManager : MonoBehaviour
 
     public GameObject[] spawnLocations;
 
-    public bool isPlayer1Turn = true;
+    public bool isPlayer1Turn;
+
+    public bool isPlayer1;
 
     public bool gameOver = false;
 
     private void Start()
     {
         photonView = GetComponent<PhotonView>();
-        //PhotonNetwork.Instantiate() can only instantiate objects with a PhotonView component. This prefab does not have one: Board
-    }
-
-    private void Update()
-    {
         if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
         {
             photonView.RPC("InitBoard", RpcTarget.All);
+            photonView.RPC("NewBoard", RpcTarget.All);
+        }
+
+    }
+
+    [PunRPC]
+    private void NewBoard()
+    {
+        boardState = new int[row_count, column_count];
+        objects = new GameObject[row_count, column_count];
+        isPlayer1Turn = true;
+        gameOver = false;
+        player1Ghost.SetActive(false);
+        player1Ghost.transform.position = spawnLocations[3].transform.position;
+        player2Ghost.SetActive(false);
+
+        //master client, isPlayer1Turn = true
+        if (PhotonNetwork.IsMasterClient)
+        {
+            isPlayer1 = true;
+        }
+        else
+        {
+            isPlayer1 = false;
         }
     }
+
+    private void player1TurnHover(int column){
+        player1Ghost.SetActive(true);
+        player1Ghost.transform.position = spawnLocations[column].transform.position;
+    }
+
+    private void player2TurnHover(int column){
+        player2Ghost.SetActive(true);
+        player2Ghost.transform.position = spawnLocations[column].transform.position;
+    }
+
+    public void hoverColumn(int column)
+    {
+        if (
+            boardState[0, column] == 0 &&
+            !gameOver &&
+            (
+            fallingPiece == null ||
+            fallingPiece.GetComponent<Rigidbody>().velocity == Vector3.zero
+            )
+        )
+        {
+            if (isPlayer1 && isPlayer1Turn){
+                player1TurnHover(column);
+            } else if (!isPlayer1 && !isPlayer1Turn){
+                player2TurnHover(column);
+            }
+        }
+    }
+
+    private void player1TurnSelect(int column){
+        player1Ghost.SetActive(false);
+        fallingPiece = PhotonNetwork.Instantiate("Player1", spawnLocations[column].transform.position, Quaternion.identity);
+        fallingPiece.GetComponent<Rigidbody>().velocity = new Vector3(0, 0.1f, 0);
+        objects[lastRow, column] = fallingPiece;
+        isPlayer1Turn = false;
+    }
+
+    private void player2TurnSelect(int column){
+        player2Ghost.SetActive(false);
+        fallingPiece = PhotonNetwork.Instantiate("Player2", spawnLocations[column].transform.position, Quaternion.identity);
+        fallingPiece.GetComponent<Rigidbody>().velocity = new Vector3(0, 0.1f, 0);
+        objects[lastRow, column] = fallingPiece;
+        isPlayer1Turn = true;
+    }
+
+    public void selectColumn(int column){
+        if (isPlayer1 && isPlayer1Turn){
+            player1TurnSelect(column);
+        } else if (!isPlayer1 && !isPlayer1Turn){
+            player2TurnSelect(column);
+        }
+    }
+
 
     [PunRPC]
     private void InitBoard()
     {
-        PhotonNetwork.Instantiate("Board", new Vector3(0.524f, 0.411f, 0), Quaternion.identity);
+        PhotonNetwork
+            .Instantiate("Board",
+            new Vector3(0.524f, 0.411f, 0),
+            Quaternion.identity);
     }
 
-    [PunRPC]
-    public void hoverColumn(int column)
-    {
-        
-        
-    }
 }
